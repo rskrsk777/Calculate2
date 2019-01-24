669180493fead58d2356bab27ee3d043d18fdc3a
rskrsk777 <ryoosk777@outlook.jp>
2019-01-24 14:14:57 +0900

    Initial Commit

---
+++ Calculate2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculate2
+//
+//  Created by ryosuke kubo on 2019/01/24.
+//  Copyright © 2019 ryosuke kubo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
